0f68cfe0ed61013571f957c6c9364920365fe317
nodirbek27 <nodirjon0927@gmail.com>
2023-03-03 16:41:24 +0500

    change site-header

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
-const navbarBtn = document.querySelector('.navbar-toggler');
-const mobileMenu = document.querySelector('.mobile-menu');
-navbarBtn.addEventListener('click', () => {
-  if (mobileMenu.style.display === 'flex') {
-    mobileMenu.style.display = 'none';
+const btnMenu = document.querySelector('.button-menu');
+const sitenav = document.querySelector('.mobile-menu');
+const sHeader = document.querySelector('.header')
+
+btnMenu.addEventListener('click', ()=> {
+  if (!sitenav.classList.contains('open')) {
+    sitenav.classList.add('open')
+    btnMenu.innerHTML = `<img class="close" src="img/close.png" alt="menu-icon" width="40" height="40">`
   } else {
-    mobileMenu.style.display = 'flex';
+    sitenav.classList.remove('open')
+    btnMenu.innerHTML = `<img class="menu" src="img/menu-icon.png" alt="menu-icon" width="40" height="40">`
   }
-});
+})
